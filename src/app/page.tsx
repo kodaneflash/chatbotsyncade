87e22b0867c6f627b4800664b1042174f7dd548e
kodaneflash <jamesonmarcher@gmail.com>
2024-05-04 07:45:50 -0400

    dd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,17 +15,16 @@ export default function Home() {
       <div className="flex items-center gap-4">
         <Image src={logo} alt="FlowBrain logo" width={100} height={100} />
         <span className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Syncade
+          Synade
         </span>
       </div>
       <p className="max-w-prose text-center">
         An intelligent note-taking app with AI integration, built with OpenAI,
         Pinecone, Next.js, Shadcn UI, Clerk, and more.
       </p>
-      <Button size="lg">
-      <Link href="/notes" legacyBehavior>Open</Link>
+      <Button size="lg" asChild>
+        <Link href="/notes">Open</Link>
       </Button>
-
     </main>
   );
 }
