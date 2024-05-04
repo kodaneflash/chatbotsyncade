35a48d146d5230f73323e27f7d7c61afa13e9563
kodaneflash <jamesonmarcher@gmail.com>
2024-05-04 06:56:49 -0400

    ee

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  titleade: "Syncade",
+  title: "Syncade",
   description: "An intelligent AI-assisted note-taking app",
 };
 
